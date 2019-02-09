79f2c9a9dfc846e0651936b06f00731e039da872
Alycia <akbutterworth@live.com>
2019-02-09 11:59:16 -0800

    Add mirror function docstring

---
+++ AutoTransformPy/mirror.py
@@ -0,0 +1,20 @@
+def mirror(image_path, direction = 'all'):
+  """Returns an array of image(s) that are the mirrored form of the original image
+
+    Mirror takes the path to an image and generates a mirrored version of that image in the horizontal direction, vertical direction, or both.
+    It returns an array of pixel values for the mirrored image(s).
+
+    Inputs:
+    -------
+    image_path: string
+        The file path of the image to be mirrored.
+    direction: string
+        Direction of mirroring.
+        Options: 'horizontal', 'vertical', 'all'
+        Default: 'all'
+
+    Returns:
+    --------
+    mirrored_images: array
+    """
+
