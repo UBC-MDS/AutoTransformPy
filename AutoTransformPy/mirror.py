e3d1ffc0af58b234ef57f182e3163129e503e3aa
Alycia <akbutterworth@live.com>
2019-02-12 16:25:28 -0800

    Fixed the code for 'all' direction

--- AutoTransformPy/mirror.py
+++ AutoTransformPy/mirror.py
@@ -1,10 +1,19 @@
+# Copyright 2019 Alycia Butterworth
+# Licensed under the MIT License
+# Use of this file must be in compliance with the License, LICENSE.md
+#
+# February 2019
+# This script is for AutoTransformPy function Mirror.
+
+
 import numpy as np
 from skimage.ip import imread
 
 def mirror(image_path, direction = 'all'):
   """Returns an array of image(s) that are the mirrored form of the original image
 
-    Mirror takes the path to an image and generates a mirrored version of that image in the horizontal direction, vertical direction, or both.
+    Mirror takes the path to an image and generates a mirrored version of that image
+    in the horizontal direction, vertical direction, or both.
     It returns an array of pixel values for the mirrored image(s).
 
     Inputs:
@@ -23,20 +32,19 @@ def mirror(image_path, direction = 'all'):
 
     # import image as array
     img = imread(image_path)
+    mirrored_images = []
 
     # flip horizontally
-    if direction == 'horizontal':
+    if direction == 'horizontal' or direction == 'all':
         horiz_image = np.fliplr(img)
-        mirrored_images = horiz_image
+        mirrored_images.append(horiz_image)
 
     # flip vertically
-    elif direction == 'vertical':
+    if direction == 'vertical' or direction == 'all':
         vert_image = img[::-1]
-        mirrored_images = vert_image
+        mirrored_images.append(vert_image)
 
-    else:
-        horiz_image = np.fliplr(img)
-        vert_image = img[::-1]
-        mirrored_images = horiz_image, vert_image
+    # convert mirrored_images back to an array
+    mirrored_images = np.asarray(mirrored_images)
 
     return mirrored_images
