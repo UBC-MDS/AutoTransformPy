ccbaf19766465a120c554f93996fb1aa86893018
Rayce Rossum <raycer>
2019-02-17 14:20:43 -0800

    Add translation source and tests

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,18 +1,22 @@
 import sys
-sys.path.append("../AutoTransformPy")
-import AutoTransformPy as pre
+import pytest
+from skimage.io import imread
+sys.path.append("../AutoTransformPy/")
+import translate as trans
 
 def test_inputs():
     with pytest.raises(TypeError):
-       pre.translate(6, 5, 2) # Not a string for the file path
-       pre.translate("Path", "seven", 2) # Not a valid number of images
-       pre.translate("Path", 6, "eight") # Not a valid translation amount
+       trans.translate(6, 5, 2) # Not a string for the file path
+       trans.translate("../tests/imgs/milad.jpg", "seven", 2) # Not a valid number of images
+       trans.translate("../tests/imgs/milad.jpg", 6, "eight") # Not a valid translation amount
 
     with pytest.raises(ValueError):
-        pre.translate("PathTo300x300Image", 7, 301) # Outside of the translation range, possible to get empty images
+        trans.translate("../tests/imgs/milad.jpg", 7, 1000) # Outside of the translation range, possible to get empty images
 
-def return_imgs(): # Tests that the number of images returned from translate is correct
-    test_img = skimage.io.imread("test_img")
-    returned_arr = pre.translate("test_image", 5, 10)
-    assert returned_arr.shape()[0] == 5
-    assert returned_arr.shape()[1:] == test_img.shape()
+def test_return_imgs(): # Tests that the number of images returned from translate is correct
+    test_img = imread("../tests/imgs/milad.jpg")
+    returned_arr, org_image = trans.translate("../tests/imgs/milad.jpg", 5, 10)
+
+    assert org_image.shape == test_img.shape
+    assert returned_arr.shape[0] == 5
+    assert returned_arr.shape[1:] == test_img.shape
