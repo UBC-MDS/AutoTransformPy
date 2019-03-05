764a483a5292f01b93ec0bcfa51554e46eb05e2d
everittb <brenden.everitt@gmail.com>
2019-03-05 15:15:01 -0800

    Second attempt at travis

--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -3,22 +3,23 @@ import pytest
 from skimage.io import imread
 sys.path.append("../AutoTransformPy/")
 import rotate as rot
+import AutoTransformPy as ATP
 
 def test_inputs():
     with pytest.raises(TypeError):
-       rot.rotate(6, 5, 345) # Not a string for the file path
-       rot.rotate("../tests/imgs/milad.jpg", "seven", 345) # Not a valid number of images
-       rot.rotate("../tests/imgs/milad.jpg", 6, "eight") # Not a valid rotation amount
+       ATP.rotate(6, 5, 345) # Not a string for the file path
+       ATP.rotate("../tests/imgs/milad.jpg", "seven", 345) # Not a valid number of images
+       ATP.rotate("../tests/imgs/milad.jpg", 6, "eight") # Not a valid rotation amount
 
     with pytest.raises(ValueError):
-        rot.rotate("../tests/imgs/milad.jpg", 7, 500) # Outside of the rotation range
-        rot.rotate("../tests/imgs/milad.jpg", -1, 500) # Nonsense number of images to return
+        ATP.rotate("../tests/imgs/milad.jpg", 7, 500) # Outside of the rotation range
+        ATP.rotate("../tests/imgs/milad.jpg", -1, 500) # Nonsense number of images to return
 
     with pytest.raises(FileNotFoundError):
-        rot.rotate("../tests/imgs/Path.jpg") # Incorrect directory/file not in location
+        ATP.rotate("../tests/imgs/Path.jpg") # Incorrect directory/file not in location
 
 def test_return_imgs(): # Tests that the number of images returned from rotate is correct
     test_img = imread("../tests/imgs/milad.jpg")
-    returned_arr = rot.rotate("../tests/imgs/milad.jpg", 5, 180)
+    returned_arr = ATP.rotate("../tests/imgs/milad.jpg", 5, 180)
     assert returned_arr.shape[0] == 6
     assert returned_arr.shape[1:] == test_img.shape
