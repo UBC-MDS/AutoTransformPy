fe402f306de5716a52bbbc2a831ad4e2bea0ea2e
everittb <brenden.everitt@gmail.com>
2019-03-06 06:55:13 -0800

    Trying Alycia recommend fix

--- tests/test_rotate.py
+++ tests/test_rotate.py
@@ -4,21 +4,23 @@ import pytest
 from skimage.io import imread
 from AutoTransformPy import rotate as rot
 
+fname = os.path.join(os.path.dirname(__file__), '../tests/imgs/milad.jpg')
+
 def test_inputs():
     with pytest.raises(TypeError):
        rot.rotate(6, 5, 345) # Not a string for the file path
-       rot.rotate("../../AutoTransformPy/tests/imgs/milad.jpg", "seven", 345) # Not a valid number of images
-       rot.rotate("../../AutoTransformPy/tests/imgs/milad.jpg", 6, "eight") # Not a valid rotation amount
+       rot.rotate(fname, "seven", 345) # Not a valid number of images
+       rot.rotate(fname, 6, "eight") # Not a valid rotation amount
 
     with pytest.raises(ValueError):
-        rot.rotate("../../AutoTransformPy/tests/imgs/milad.jpg", 7, 500) # Outside of the rotation range
-        rot.rotate("../../AutoTransformPy/tests/imgs/milad.jpg", -1, 500) # Nonsense number of images to return
+        rot.rotate(fname, 7, 500) # Outside of the rotation range
+        rot.rotate(fname, -1, 500) # Nonsense number of images to return
 
     with pytest.raises(FileNotFoundError):
         rot.rotate("../tests/imgs/Path.jpg", 7, 100) # Incorrect directory/file not in location
 
 def test_return_imgs(): # Tests that the number of images returned from rotate is correct
-    test_img = imread("../../AutoTransformPy/tests/imgs/milad.jpg")
-    returned_arr = rot.rotate("../../AutoTransformPy/tests/imgs/milad.jpg", 5, 180)
+    test_img = imread(fname)
+    returned_arr = rot.rotate(fname, 5, 180)
     assert returned_arr.shape[0] == 6
     assert returned_arr.shape[1:] == test_img.shape
--- tests/test_translate.py
+++ tests/test_translate.py
@@ -1,21 +1,23 @@
 import sys
+import os
 import pytest
 from skimage.io import imread
-sys.path.append("../AutoTransformPy/")
 from AutoTransformPy import translate as trans
+
+fname = os.path.join(os.path.dirname(__file__), '../tests/imgs/milad.jpg')
 
 def test_inputs():
     with pytest.raises(TypeError):
        trans.translate(6, 5, 2) # Not a string for the file path
-       trans.translate("/AutoTransformPy/tests/imgs/milad.jpg", "seven", 2) # Not a valid number of images
-       trans.translate("/AutoTransformPy/tests/imgs/milad.jpg", 6, "eight") # Not a valid translation amount
+       trans.translate(fname, "seven", 2) # Not a valid number of images
+       trans.translate(fname, 6, "eight") # Not a valid translation amount
 
     with pytest.raises(ValueError):
-        trans.translate("/AutoTransformPy/tests/imgs/milad.jpg", 7, 1000) # Outside of the translation range, possible to get empty images
+        trans.translate(fname, 7, 1000) # Outside of the translation range, possible to get empty images
 
 def test_return_imgs(): # Tests that the number of images returned from translate is correct
-    test_img = imread("/AutoTransformPy/tests/imgs/milad.jpg")
-    returned_arr = trans.translate("/AutoTransformPy/tests/imgs/milad.jpg", 5, 10)
+    test_img = imread(fname)
+    returned_arr = trans.translate(fname, 5, 10)
 
     assert returned_arr[0].shape == test_img.shape
     assert returned_arr.shape[0] == 6
