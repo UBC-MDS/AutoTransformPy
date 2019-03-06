e44ab1a7b2c89136f7a74d5095bc9d7bf55f4113
everittb <brenden.everitt@gmail.com>
2019-03-05 16:49:40 -0800

    Trying to fix travis path issues

--- tests/test_translate.py
+++ tests/test_translate.py
@@ -7,15 +7,15 @@ from AutoTransformPy import translate as trans
 def test_inputs():
     with pytest.raises(TypeError):
        trans.translate(6, 5, 2) # Not a string for the file path
-       trans.translate("imgs/milad.jpg", "seven", 2) # Not a valid number of images
-       trans.translate("imgs/milad.jpg", 6, "eight") # Not a valid translation amount
+       trans.translate("AutoTransformPy/tests/imgs/milad.jpg", "seven", 2) # Not a valid number of images
+       trans.translate("AutoTransformPy/tests/imgs/milad.jpg", 6, "eight") # Not a valid translation amount
 
     with pytest.raises(ValueError):
-        trans.translate("imgs/milad.jpg", 7, 1000) # Outside of the translation range, possible to get empty images
+        trans.translate("AutoTransformPy/tests/imgs/milad.jpg", 7, 1000) # Outside of the translation range, possible to get empty images
 
 def test_return_imgs(): # Tests that the number of images returned from translate is correct
-    test_img = imread("imgs/milad.jpg")
-    returned_arr = trans.translate("imgs/milad.jpg", 5, 10)
+    test_img = imread("AutoTransformPy/tests/imgs/milad.jpg")
+    returned_arr = trans.translate("AutoTransformPy/tests/imgs/milad.jpg", 5, 10)
 
     assert returned_arr[0].shape == test_img.shape
     assert returned_arr.shape[0] == 6
